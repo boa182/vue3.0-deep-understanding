4c313cbfdebba0d000b2e2ac82cf7b098585030e
boa <@boa.com>
2022-04-30 10:18:22 +0800

    effct()和reactive()

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <effectReactive msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import effectReactive from './components/effect-reactive.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    effectReactive
   }
 }
 </script>
